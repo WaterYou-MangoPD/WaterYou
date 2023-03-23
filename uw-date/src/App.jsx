601b45eb42aed7712942843320f97010b71e57b9
Bleumm <pan.jason12@gmail.com>
2023-03-23 14:23:29 -0400

    initialized react app

---
+++ uw-date/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MainMenu from './components/main_menu.jsx';
+
+function App() {
+  return (
+    <div className="App">
+        <MainMenu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ uw-date/src/components/main_menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './main_menu.css';
+
+function main_menu() {
+  return (
+    <div className='title'>
+      WaterWho?
+    </div>
+  )
+}
+
+export default main_menu;
